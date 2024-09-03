0cd9976bf8237d307848ecbc1bb21b20d71ee81f
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-09-04 02:01:01 +0300

    Add biogaphy

--- src/components/Biography.jsx
+++ src/components/Biography.jsx
@@ -1,11 +1,47 @@
 const Biography = () => {
   return (
     <section id='biography' className='bg-creamWhite py-10 text-darkBlue'>
-      <div className='container mx-auto'>
+      <div className='container mx-auto px-4'>
         <h3 className='text-3xl font-bold mb-4'>Biography</h3>
-        <p className='text-lg'>
-          Isaac Ngaruiku Matu was born in... [Detailed biography goes here]
+        <p className='text-lg py-4'>
+          Mzee Ngariuku was born in 10<sup>th</sup> October 1928 at Gachika
+          village, Kiganjo, Nyeri County. He was the last born of the Late Mzee
+          Matu and Mama Kabugo. Since his childhood, he has been obidient and
+          respectful to his parents.
         </p>
+        <p className='py-4 text-lg'>
+          Grewing up at times when there wasn't much education, Mzee Ngariuku
+          attained his highest education in standard two at Kiruchu Primary
+          School. Immediately, he started working as a casual labourer.
+        </p>
+        <p className='py-4 text-lg'>
+          During this time, the Mau Mau war was in it's best. Without even being
+          an active fighter, he found himself and many others contributing to
+          the early struggle for independence. Him and many others were
+          detained, what is popularly known as{" "}
+          <strong>(Gutwarwo ithamirioni)</strong>. In their detainment, they
+          were forced to work under the colonial government as casual labourers.
+          They participated in various constructions and development projects
+          carried out by the colonial government including the construction of
+          The Jomo Kenyatta International Airport. They worked as slaves
+          following their failure to collaborate with the{" "}
+          <strong>Mzungu</strong>. He never collaborated with them unlike the so
+          called <strong>Kamatimu/Ngati</strong>.
+        </p>
+        <p className='py-4 text-lg'>
+          Till his demise, Mzee Ngariuku has lived to enjoy the fruits of their
+          struggle. He kept reminding us that those who fought for independece,
+          they did so, so that future generations can live a better life in a
+          better country. He also discouraged the selling of Land, and he could
+          quote{" "}
+          <strong>
+            Kai teri uyo mutoi ni murume, njamba cia wiyathi cikuaga irumite
+            teri na gudi
+          </strong>{" "}
+          meaning those who fought for the independence could never want other
+          generations to sell their lands.
+        </p>
+        <p className='py-4 text-lg'>He lived with his believes and kept the oath they once took during the Mau Mau war; <strong>(Kunyua muma)</strong> and at some points he couldn't tell us more about what was happening in those old days. He was a truthful, honest man and with much wisdom such that he has impacted and will continue to impact our decisions in life as his children, grand-children and great grand-children.</p>
       </div>
     </section>
   );
